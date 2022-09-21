e615fff82e6a86551a2f3ec412a7e3b0ef2fc1c1
Ahmidi Yasser <88626454+dexter21767@users.noreply.github.com>
2022-09-21 16:00:50 +0100

    first commit

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 var env = process.env.NODE_ENV ? 'beamup':'local';
 
 var config = {
-    BaseURL: "https://subscene.com",
+    BaseURL: "https://www.addic7ed.com",
     APIURL: 'https://api.themoviedb.org/3'
 }
 
 switch (env) {
     case 'beamup':
 		config.port = process.env.PORT
-        config.local = "https://2ecbbd610840-subscene.baby-beamup.club"
+        config.local = "https://2ecbbd610840-addic7ed.baby-beamup.club"
         break;
 
     case 'local':
